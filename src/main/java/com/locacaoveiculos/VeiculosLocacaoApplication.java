890bca27a1533b506a173e6793f17c9c3644a4f4
André Guzzi <andre_guzzi@hotmail.com>
2022-09-20 16:42:52 -0300

    versão 1.0

---
+++ src/main/java/com/locacaoveiculos/VeiculosLocacaoApplication.java
@@ -0,0 +1,13 @@
+package com.locacaoveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeiculosLocacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeiculosLocacaoApplication.class, args);
+	}
+
+}
